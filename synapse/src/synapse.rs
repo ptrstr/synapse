d26f53714c7e7fa97e522cfaa744225fb95463b6
ptrstr <ptrstr@protonmail.com>
2020-03-07 16:12:44 +0000

    Working but training requires rewrite
    Example: Simple XOR gives around 0.5 with 20000 trainings

--- synapse/src/synapse.rs
+++ synapse/src/synapse.rs
@@ -1,10 +1,11 @@
 extern crate rand;
 use rand::Rng;
 use crate::neuron;
+use crate::utils;
 
 pub struct Synapse {
-	neurons: Vec<Vec<neuron::Neuron>>,
-	weights: Vec<Vec<Vec<f64>>>,
+	pub neurons: Vec<Vec<neuron::Neuron>>,
+	pub weights: Vec<Vec<Vec<f64>>>,
 }
 
 impl Synapse {
@@ -34,42 +35,67 @@ impl Synapse {
 			}
 		}
 
+
 		Synapse {
 			neurons: neurons,
 			weights: weights,
 		}
 	}
 
-	pub fn guess(&self, inputs: Vec<f64>) -> Vec<f64> {
+	fn feed_forward(&self, inputs: Vec<f64>) -> Vec<Vec<f64>> {
 		let mut artifacts:Vec<Vec<f64>> = vec![];
 		artifacts.push(inputs);
 		for i in 0..self.neurons.len() - 1 {
 			let mut new_artifacts:Vec<f64> = vec![];
-			for j in 0..self.neurons[i].len() {
-				new_artifacts.push(self.neurons[i+1][j].run(artifacts[i].clone(), self.weights[i][j].clone()));
+			for j in 0..self.neurons[i+1].len() {
+				let mut current_weights:Vec<f64> = vec![];
+				for k in 0..self.neurons[i].len() {
+					current_weights.push(self.weights[i][k][j]);
+				}
+				new_artifacts.push(self.neurons[i+1][j].run(artifacts[i].clone(), current_weights));
 			}
 			artifacts.push(new_artifacts);
 		}
-		println!("{:?}", artifacts);
-		return artifacts[artifacts.len() - 1].clone();
+		return artifacts;
 	}
 
-	pub fn train(&mut self, inputs: Vec<Vec<f64>>, outputs: Vec<Vec<f64>>, iterations: usize, learning_rate: f64) {
-		let mut rng = rand::thread_rng();
-		for _ in 0..iterations {
-			let current_io = rng.gen_range(0, inputs.len());
-			let guess_outputs = self.guess(inputs[current_io].clone());
-			let mut error_outputs:Vec<f64> = vec![];
-			for i in 0..outputs[current_io].len() {
-				error_outputs.push(outputs[current_io][i] - guess_outputs[i]);
-			}
+	pub fn guess(&self, inputs: Vec<f64>) -> Vec<f64> {
+		let guesses = self.feed_forward(inputs);
+		return guesses[guesses.len() - 1].clone();
+	}
 
-			let mut errors:Vec<Vec<f64>> = vec![];
-			for i in self.neurons.len() - 1..=0 {
-				
+	pub fn train(&mut self, inputs: Vec<f64>, outputs: Vec<f64>, learning_rate: f64) {
+		let guesses:Vec<Vec<f64>> = self.feed_forward(inputs);
+
+		// Errors are in reverse order
+		let mut errors:Vec<Vec<f64>> = vec![vec![]];
+		for i in 0..outputs.len() {
+			errors[0].push(outputs[i] - guesses[guesses.len() - 1][i]);
+		}
+
+		for cur_layer in (1..self.neurons.len()).rev() {
+			let mut gradients:Vec<f64> = guesses[cur_layer].clone();
+			let mut weight_deltas:Vec<Vec<f64>> = vec![];
+			for cur_neuron in 0..self.neurons[cur_layer].len() {
+				gradients[cur_neuron] = utils::dsigmoid(gradients[cur_neuron]) * errors[errors.len() - 1][cur_neuron] * learning_rate;
+				self.neurons[cur_layer][cur_neuron].add_bias(gradients[cur_neuron]);
 			}
-			for i in 0..error_outputs.len() {
-				println!("{}: [Guess: {}], [Target: {}], [Error: {}]", i, guess_outputs[i], outputs[current_io][i], error_outputs[i]);
+			
+			errors.push(vec![0.0; self.neurons[cur_layer-1].len()]);
+			for prev_neuron in 0..self.neurons[cur_layer-1].len() {
+				weight_deltas.push(vec![]);
+				
+				for cur_neuron in 0..self.neurons[cur_layer].len() {
+					weight_deltas[prev_neuron].push(gradients[cur_neuron] * guesses[cur_layer - 1][prev_neuron]);
+				}
+				
+				for prev_weight in 0..self.weights[cur_layer-1][prev_neuron].len() {
+					self.weights[cur_layer-1][prev_neuron][prev_weight] += weight_deltas[prev_neuron][prev_weight];
+					
+					let new_error = errors[errors.len() - 2][prev_weight] * self.weights[cur_layer-1][prev_neuron][prev_weight];
+					let cur_error_layer = errors.len() - 1;
+					errors[cur_error_layer][prev_neuron] += new_error;
+				}
 			}
 		}
 	}
--- synapse_test/src/main.rs
+++ synapse_test/src/main.rs
@@ -1,6 +1,30 @@
 extern crate synapse;
 
 fn main() {
-	let mut x = synapse::synapse::Synapse::new(vec![2,2,2]);
-	x.train(vec![vec![4.0,6.0]], vec![vec![3.0, 5.0]], 1,0.5);
+/*
+	let mut x = synapse::synapse::Synapse::new(vec![2,2,1]);
+	for _ in 0..20000 {
+		x.train(vec![0.0,0.0], vec![0.0], 0.5);
+		x.train(vec![0.0,1.0], vec![1.0], 0.5);
+		x.train(vec![1.0,0.0], vec![1.0], 0.5);
+		x.train(vec![1.0,1.0], vec![0.0], 0.5);
+	}
+	let mut guess_inputs:Vec<f64> = vec![0.0,1.0];
+	println!("{} XOR {} = {}", guess_inputs[0], guess_inputs[1], x.guess(guess_inputs.clone())[0]);
+	guess_inputs = vec![0.0,0.0];
+	println!("{} XOR {} = {}", guess_inputs[0], guess_inputs[1], x.guess(guess_inputs.clone())[0]);
+*/
+	let mut x = synapse::synapse::Synapse::new(vec![1,1,1]);
+	let mut y = x.neurons[1][0].get_bias();
+	x.neurons[1][0].add_bias(-1_f64 * y);
+	x.neurons[1][0].add_bias(0.2);
+
+	y = x.neurons[2][0].get_bias();
+	x.neurons[2][0].add_bias(-1_f64 * y);
+	x.neurons[2][0].add_bias(0.5);
+	
+	x.weights[0][0][0] = 0.6;
+	x.weights[1][0][0] = 0.5;
+
+	println!("Guess: {:?}", x.guess(vec![28_f64]));
 }
